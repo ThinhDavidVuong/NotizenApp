06add37cf51592714f6fef95dca5a93f1b65e2fc
ThinhDavidVuong <thinh_david_vuong@hotmail.com>
2017-05-23 08:40:45 +0200

    Created Project

---
+++ NotizenApp/app/src/main/java/ch/bbcag/notizenapp/NotizenApp.java
@@ -0,0 +1,13 @@
+package ch.bbcag.notizenapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotizenApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notizen_app);
+    }
+}
